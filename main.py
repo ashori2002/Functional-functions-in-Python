a71f6c0185db0cbccc31ea193956faaacf8a163d
ashori2002 <mohammadashori2002@gmail.com>
2023-02-25 16:57:54 +0330

    map() step 2

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 
 myList = [1,2,3,4,5,6,7]
+"""
 def mines(num):
     num -= 1
     return num
 myList_new = list(map(mines,myList))
+
+print(myList_new)
+"""
+
+myList_new = list(map(lambda num : num - 1 , myList))
 
 print(myList_new)
