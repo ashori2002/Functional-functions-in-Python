aaf9a36d30a97068b739f433afe015e91fc10a4a
ashori2002 <mohammadashori2002@gmail.com>
2023-02-25 17:25:14 +0330

    filter() step 1

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ print(myList_new)
 myList_new = list(map(lambda num : num - 1 , myList))
 
 print(myList_new)
+
+###################################################################
+
+def odd(num):
+    return num % 2 == 0
+myList_new1 = list(filter(odd , myList))
+print(myList_new1)
